3a6957d69e603747ab3f72ab9a3bac09b8c5c1c6
yunkooo <yunkoooooo@gmail.com>
2024-08-03 23:59:24 +0900

    💄 [Styling] Banner carousel style

--- src/components/main/banner/Banner.tsx
+++ src/components/main/banner/Banner.tsx
@@ -1,3 +1,20 @@
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+} from '@/components/ui/Carousel';
+import BannerItem from './BannerItem';
+
 export default function Banner() {
-  return <div>Banner</div>;
+    return (
+        <Carousel>
+            <CarouselContent>
+                {Array.from({ length: 3 }).map((_, index) => (
+                    <CarouselItem key={index} className='pl-0'>
+                        <BannerItem />
+                    </CarouselItem>
+                ))}
+            </CarouselContent>
+        </Carousel>
+    );
 }
--- src/components/main/banner/BannerItem.tsx
+++ src/components/main/banner/BannerItem.tsx
@@ -1,3 +1,16 @@
+import Image from 'next/image';
+import bannerImg from '@/assets/banner_1.jpg';
+
 export default function BannerItem() {
-  return <div>BannerItem</div>;
+    return (
+        <div className='relative'>
+            <h1 className='absolute top-[100px] left-[40px] text-2xl font-bold'>
+                첫 구독 할인
+            </h1>
+            <p className='absolute top-[135px] left-[40px] text-xl'>
+                오픈기념 할인행사
+            </p>
+            <Image src={bannerImg} alt='banner image' />
+        </div>
+    );
 }
